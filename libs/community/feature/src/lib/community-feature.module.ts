a6d280626453c2a8e02334d3f286298ed691372e
NehaVel <nehavelmurugan13@gmail.comt>
2023-08-29 22:21:46 +0530

    Api integration - community page

--- libs/community/feature/src/lib/community-feature.module.ts
+++ libs/community/feature/src/lib/community-feature.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddBlogComponent } from './add-blog/add-blog.component';
 // import { CommunityService } from './community.service';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { CommunityService } from './community.service';
 const routes: Route[] = [
   {
     path: '',
@@ -41,7 +42,7 @@ const routes: Route[] = [
     AddBlogComponent,
     BlogDetailComponent,
   ],
- providers: [CommunityService],
+  providers: [CommunityService],
   exports: [CommunityComponent, RouterModule],
 })
 export class CommunityFeatureModule {}
