f6b63f383220705b041ada300cc2cac5834d4b91
NehaVel <nehavelmurugan13@gmail.comt>
2023-09-12 11:22:36 +0530

    qa detail

--- libs/blogs/ui/src/lib/blog-detail/blog-detail.component.ts
+++ libs/blogs/ui/src/lib/blog-detail/blog-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment/moment';
+import { Router } from '@angular/router';
 @Component({
   selector: 'thryve-disseminate-blog-detail-ui',
   templateUrl: './blog-detail.component.html',
@@ -10,6 +11,9 @@ export class BlogDetailComponent implements OnInit {
   yesterday = new Date();
   moment: any = moment;
   Math = Math;
+  constructor(private router: Router){
+    
+  }
   ngOnInit(){
     this.yesterday.setDate(this.todaysDate.getDate() - 1);
   }
